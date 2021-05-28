cc04bcb7f995321b19701aa9d50dc3230df0935d
bcmendoza <brian.mendoza@deciphernow.com>
2021-05-28 12:57:18 -0400

    made control-api deployment

--- create-docker-secret.sh
+++ create-docker-secret.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 kubectl create secret docker-registry docker.secret \
+  -n gm-operator-system \
+  --docker-server=docker.greymatter.io \
+  --docker-username=$NEXUS_USER \
+  --docker-password=$NEXUS_PASSWORD \
+  --docker-email=$NEXUS_USER
+
+kubectl create secret docker-registry docker.secret \
   --docker-server=docker.greymatter.io \
   --docker-username=$NEXUS_USER \
   --docker-password=$NEXUS_PASSWORD \
