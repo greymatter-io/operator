59e0de52c8f4f3024915a81d03c974afd9f179be
bcmendoza <brian.mendoza@deciphernow.com>
2021-09-23 13:51:43 -0400

    System -> Installer

--- pkg/gmcore/install.go
+++ pkg/gmcore/install.go
@@ -11,7 +11,7 @@ import (
 // for their deployments and statefulsets, as well as sidecar injection for their pods.
 // If auto-inject is enabled (default=true), labels each existing appsv1.Deployment/StatefulSet
 // plus their pod templates so that those workloads are added to the mesh automatically.
-func (s *System) ApplyMesh(c client.Client, mesh v1alpha1.Mesh) {
+func (i *Installer) ApplyMesh(c client.Client, mesh v1alpha1.Mesh) {
 	// TODO: Get the appropriate s.values InstallValuesConfig, DeepCopy it, and then apply options
 	// NOTE: v1alpha1.Mesh should have a method that reads its spec and returns InstallValues options to overlay.
 }
@@ -19,10 +19,10 @@ func (s *System) ApplyMesh(c client.Client, mesh v1alpha1.Mesh) {
 // Removes all resources created for installing Grey Matter core components of a mesh.
 // Also removes the mesh from labels of resources (removing the labels if no more meshes are listed)
 // i.e. namespaces, deployments, statefulsets, and pods (via pod templates).
-func (s *System) RemoveMesh(c client.Client, name string) {
+func (i *Installer) RemoveMesh(c client.Client, name string) {
 }
 
 // Given a slice of corev1.Containers, injects sidecar(s) to enable traffic for each mesh specified.
-func (s *System) Inject(containers []corev1.Container, xdsCluster string, meshes []string) []corev1.Container {
+func (i *Installer) Inject(containers []corev1.Container, xdsCluster string, meshes []string) []corev1.Container {
 	return containers
 }
