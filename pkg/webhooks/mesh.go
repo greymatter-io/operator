3902cd8f9e13c6498c23bfa566e9ad73d564cfa2
Kyle Gallagher <kyle.gallagher@deciphernow.com>
2021-10-22 12:27:43 -0400

    Promote Mesh CR to cluster-scoped resource (#32)
    * promote mesh cr to cluster scope and crate validation on duplicate install namespace mesh CRs
    
    * update scripts to remove pvc
    
    * update cli.go to use mehs install namespace
    
    * update per pr comments
    
    * update per pr comments
    
    Co-authored-by: Kyle Gallagher <kyle.gallagher@greymatter.io>

--- pkg/webhooks/mesh.go
+++ pkg/webhooks/mesh.go
@@ -9,6 +9,7 @@ import (
 	"github.com/greymatter-io/operator/pkg/installer"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
@@ -41,6 +42,7 @@ type meshValidator struct {
 	*installer.Installer
 	*cli.CLI
 	*admission.Decoder
+	ctrlclient.Client
 }
 
 // Implements admission.DecoderInjector.
@@ -65,25 +67,36 @@ func (mv *meshValidator) Handle(ctx context.Context, req admission.Request) admi
 		return admission.ValidationResponse(false, "attempted to create Mesh in 'gm-operator' namespace")
 	}
 
+	// Ensure that each mesh spec has a unique install-namespace
+	meshList := &v1alpha1.MeshList{}
+	if err := mv.List(context.TODO(), meshList); err != nil {
+		return admission.ValidationResponse(false, "Unable to get list of existing mesh resources")
+	}
+	// parse through meshes  and see if the request object Namespace is already in a mesh spec
+	obj := &v1alpha1.Mesh{}
+	if err := mv.DecodeRaw(req.Object, obj); err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	for _, mesh := range meshList.Items {
+		if mesh.Spec.InstallNamespace == obj.Spec.InstallNamespace {
+			return admission.ValidationResponse(false, "A mesh exists with the specified install_namespace already.")
+		}
+	}
+
 	// TODO: Ensure only one mesh exists in a namespace
 	// TODO: Ensure namespace doesn't belong to another Mesh (as a WatchNamespace)
 	// TODO: Ensure Mesh watch namespaces are unique
 
-	mesh := &v1alpha1.Mesh{}
-	if err := mv.Decode(req, mesh); err != nil {
-		return admission.Errored(http.StatusInternalServerError, err)
-	}
-
-	go mv.ConfigureMeshClient(mesh)
+	go mv.ConfigureMeshClient(obj)
 
 	if req.Operation == admissionv1.Create {
-		go mv.ApplyMesh(nil, mesh)
+		go mv.ApplyMesh(nil, obj)
 	} else {
 		prev := &v1alpha1.Mesh{}
 		if err := mv.DecodeRaw(req.OldObject, prev); err != nil {
 			return admission.Errored(http.StatusInternalServerError, err)
 		}
-		go mv.ApplyMesh(prev, mesh)
+		go mv.ApplyMesh(prev, obj)
 	}
 
 	return admission.ValidationResponse(true, "allowed")
