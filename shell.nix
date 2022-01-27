1c2965e32213679c9e82028f4d61744cbb7cef5b
John Cudd <john.cudd@greymatter.io>
2022-01-27 18:13:53 -0500

    Somewhat working nix with kind

--- shell.nix
+++ shell.nix
@@ -6,11 +6,12 @@ mkShell {
     pkgs.kompose
     pkgs.killall
     pkgs.coreutils
-    pkgs.kube3d
+    #pkgs.kube3d
+    pkgs.kind
   ];
 
   shellHook = ''
-trap 'k3d cluster stop gm-cluster; k3d cluster delete gm-cluster;  killall kubectl;' EXIT
+trap 'kind stop cluster --name gm-cluster; kind delete cluster --name gm-cluster;  killall kubectl;' EXIT
 docker --version
 if docker --version; then
   echo "Docker Daemon is running" 
@@ -21,73 +22,18 @@ fi
 
 docker login docker.greymatter.io
 
-k3d cluster create gm-cluster -a 1 -p 30000:10808@loadbalancer
-k3d cluster start gm-cluster
+kind create cluster --name gm-cluster
 kubectl get nodes
 
+#k3d cluster create gm-cluster -a 1 -p 30000:10808@loadbalancer
+#k3d cluster start gm-cluster
+
 #Make sure we are using the right context
-kubectl config use-context k3d-cluster
-
-# Install k8s dashboard
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
-kubectl proxy &
-
-echo "apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard" | kubectl apply -f -
-
-echo "apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard" | kubectl apply -f - 
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac OSX
-  if [[ -z "$GREYMATTER_REGISTRY_USERNAME" ]]; then
-    echo "Docker Username(email): "
-    read GREYMATTER_REGISTRY_USERNAME
-  fi
-  if [[ -z "$GREYMATTER_REGISTRY_PASSWORD" ]]; then
-    echo "Docker Password: "
-    read -s GREYMATTER_REGISTRY_PASSWORD
-  fi
-  kubectl create secret docker-registry regcred \
-    --docker-server=docker.greymatter.io \
-    --docker-username=$GREYMATTER_REGISTRY_USERNAME \
-    --docker-password=$GREYMATTER_REGISTRY_PASSWORD \
-    --docker-email=$GREYMATTER_REGISTRY_USERNAME \
-    -n default
-else
-  kubectl create secret generic regcred \
-   --from-file=.dockerconfigjson=$HOME/.docker/config.json \
-   --type=kubernetes.io/dockerconfigjson \
-   -n default
-fi
-
-echo "Just a moment..."
-sleep 5;
-echo "Adding private credentials to service account."
-kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
-
-
-echo -e "\n\nLogin to the dashboard with token:"
-echo -e "\n\thttp://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/workloads?namespace=_all#/login\n"
-echo -e "\nKubernetes Dashboard Token:\n"
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
-echo -e "\n\n"
+kubectl config use-context kind-gm-cluster
 
 # Create namespace
 kubectl create namespace gm-operator
+sleep 5;
 
 # Create docker pull secrets from local docker config
 if [[ "$OSTYPE" == "darwin"* ]]; then
