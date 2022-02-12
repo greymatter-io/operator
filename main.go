greymatter-io/operator

commit ee4f5583a245a2899e6fea937df87c6d2300cba0

Rename app-specific config flag to configPath
This prevents a redeclaration of a flag, avoids a panic.

main.go
@@ -62,8 +62,8 @@ func init() {
 
 // Global config flags
 var (
-	configFile  string
-	development bool
+	configPath string
+	zapDevMode bool
 )
 
 func main() {
@@ -76,16 +76,16 @@ func main() {
 func run() error {
 	flag.Parse()
 
-	flag.StringVar(&configFile, "config", "", "The operator will load its initial configuration from this file if defined.")
-	flag.BoolVar(&development, "development", false, "Run in development mode.")
+	flag.StringVar(&configPath, "configPath", "", "The operator will load its initial configuration from this file if defined.")
+	flag.BoolVar(&zapDevMode, "zapDevMode", false, "Configure zap logger in development mode.")
 
 	// Bind flags for Zap logger options.
-	opts := zap.Options{Development: development}
+	opts := zap.Options{Development: zapDevMode}
 	opts.BindFlags(flag.CommandLine)
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	// Initialize operator options with set values.
-	// These values will not be replaced by any values set in a read configFile.
+	// These values will not be replaced by any values set in a read configPath.
 	options := ctrl.Options{
 		Scheme:                  scheme,
 		LeaderElection:          true,
@@ -105,15 +105,15 @@ func run() error {
 		ClusterIngressName: "cluster",
 	}
 
-	// Attempt to read a configFile if one has been configured.
+	// Attempt to read a configPath if one has been configured.
 	cfg := defaultBootstrapConfig
 	var err error
-	if configFile != "" {
-		options, err = options.AndFrom(ctrl.ConfigFile().AtPath(configFile).OfKind(&cfg))
+	if configPath != "" {
+		options, err = options.AndFrom(ctrl.ConfigFile().AtPath(configPath).OfKind(&cfg))
 		if err != nil {
-			return fmt.Errorf("failed to load bootstrap config at path %s: %w", configFile, err)
+			return fmt.Errorf("failed to load bootstrap config at path %s: %w", configPath, err)
 		}
-		logger.Info("Loaded bootstrap config", "Path", configFile)
+		logger.Info("Loaded bootstrap config", "Path", configPath)
 	}
 
 	// Start up our CFSSL server for issuing two certs:

config/base/deployment_config_patch.yaml
@@ -9,7 +9,7 @@ spec:
       containers:
       - name: operator
         args:
-        - "--config=config/bootstrap_config.yaml"
+        - "--configPath=config/bootstrap_config.yaml"
         volumeMounts:
         - name: operator-config
           mountPath: /app/config/bootstrap_config.yaml

commit 7c954ab9e45c2ee12f2b00a7bd9ccd77d6557424

Call parse late
Fixes bug introduced in ee4f5583a245a2899e6fea937df87c6d2300cba0

main.go
@@ -74,7 +74,6 @@ func main() {
 }
 
 func run() error {
-	flag.Parse()
 
 	flag.StringVar(&pprofAddr, "pprofAddr", ":1234", "Address for pprof server; has no effect on release builds")
 	flag.StringVar(&configPath, "configPath", "", "The operator will load its initial configuration from this file if defined.")
@@ -84,6 +83,9 @@ func run() error {
 	opts := zap.Options{Development: zapDevMode}
 	opts.BindFlags(flag.CommandLine)
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	// We have to call Parse late for some reason
+	flag.Parse()
 
 	// Initialize operator options with set values.
 	// These values will not be replaced by any values set in a read configPath.