0224fe49e941872b0b7c0e73027c7cba5048f13b
Abhay Mathur <abhay.mathur@greymatter.io>
2022-07-13 08:55:10 -0400

    Replaced Decipher Technology Studios instances with Greymatter

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,5 +1,5 @@
 /*
-Copyright Decipher Technology Studios 2021.
+Copyright greymatter.io 2022.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
